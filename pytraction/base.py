1c8e38ccaf298a1fe8a04075ad473db218622e14
Jindrich Luza <jluza@redhat.com>
2023-02-24 21:23:25 +0100

    Dataclasses

--- pytraction/base.py
+++ pytraction/base.py
@@ -14,13 +14,16 @@ from typing import (
     get_origin,
     ForwardRef,
     Optional,
-    TypeVar
+    TypeVar,
+    Generic,
+    Tuple
 )
 
 import dataclasses
 
 
 Base = ForwardRef("Base")
+TList = ForwardRef("TList")
 
 
 class NoAnnotationError(TypeError):
@@ -73,37 +76,37 @@ class TypeNode:
         current = root
         stack = []
         while True:
-            print("TN current", current.type_)
             if hasattr(current.type_, "__args__"):
-                print("TN current args", current.type_.__args__)
                 for arg in current.type_.__args__:
-                    print("TN arg", arg)
                     n = cls(type_=arg)
                     stack.append(n)
                     current.children.append(n)
             if not stack:
                 break
             current = stack.pop()
-        print("root", root, root.children)
         return root
 
     def replace_params(self, params_map):
+        print(params_map)
         stack = [(self, 0, None)]
         post_order = []
         while stack:
             current, parent_index, current_parent = stack.pop(0)
             for n, ch in enumerate(current.children):
-                print("ch", ch)
                 stack.insert(0, (ch, n, current))
             if type(current.type_) == TypeVar:
+                print("replacing", current.type_)
                 if current.type_ in params_map:
+                    print("replaced", params_map[current.type_])
                     current.type_ = params_map[current.type_]
             post_order.insert(0, (current, parent_index, current_parent))
         for item in post_order:
             node, parent_index, parent = item
+            print("-- REPLACE", node.type_, parent_index, parent)
             if not parent:
                 continue
             parent.children[parent_index] = node
+            print(parent, parent.children)
 
     def to_type(self):
         stack = [(self, 0, None)]
@@ -116,13 +119,15 @@ class TypeNode:
 
         for item in post_order:
             node, parent_index, parent = item
+            print("TO TYPE", node, parent_index, parent, node.children)
+
+            if node.children:
+                print("children to type", node.type_)
+                node.type_ = (get_origin(node.type_) or type(node.type_))[tuple([x.type_ for x in node.children])]
+                print("children to type2", node.type_)
             if not parent:
                 continue
-            if node.children:
-                print("node ch", tuple([x.type_ for x in node.children]))
-                node.type_ = node.type_[tuple([x.type_ for x in node.children])]
             parent.children[parent_index] = node
-        print("TYPE", post_order[-1][0].type_)
         return post_order[-1][0].type_
 
 
@@ -197,9 +202,9 @@ class TypeNode:
         node = CMPNode(self, other, op, op)
         post_order = self.__eq_post_order(node)
 
-        #print("--- EQ ---")
+        print("--- EQ ---")
         for cmp_node in post_order:
-            #print("cmp node", cmp_node.op, cmp_node.n1.type_, cmp_node.n2.type_)
+            print("cmp node", cmp_node.eq, cmp_node.op, cmp_node.n1.type_, cmp_node.n2.type_)
             if cmp_node.op == "any":
                 if cmp_node.children:
                     ch_eq = any([ch.eq for ch in cmp_node.children])
@@ -211,6 +216,7 @@ class TypeNode:
             n1_type = get_origin(cmp_node.n1.type_) or cmp_node.n1.type_
             n2_type = get_origin(cmp_node.n2.type_) or cmp_node.n2.type_
 
+            print("EQ CMP", n1_type, n2_type)
             # check types only of both types are not union
             # otherwise equality was already decided by check above
             if n1_type != Union and n2_type != Union:
@@ -227,66 +233,60 @@ class TypeNode:
         else:
             op = "any"
 
-        root_node = CMPNode(self, TypeNode(Union[int, str, List, Dict, bool, Base, type(None)]), op, op)
+        root_node = CMPNode(self, TypeNode(Union[int, str, bool, Base, TList, Dict, type(None)]), op, op)
         stack = [root_node]
         post_order = []
         post_order.insert(0, root_node)
         while stack:
             current_node = stack.pop()
-            if current_node.n1.children: # and current_node.n1.type_ != Union:
+            if current_node.n1.children:
                 for ch1 in current_node.n1.children:
-                    if ch1.children: # and ch1.type_ != Union:
+                    if ch1.children:
                         op = "all"
                     else:
                         op = "any"
-                    node = CMPNode(ch1, TypeNode(Union[int, str, List, Dict, bool, Base, type(None), TypeVar("X")]), op, op)
+                    node = CMPNode(ch1, TypeNode(Union[int, str, Dict, bool, Base, TList, type(None), TypeVar("X")]), op, op)
                     stack.insert(0, node)
                     post_order.insert(0, node)
                     current_node.children.append(node)
             else:
-                for u in (int, str, List, Dict, bool, Base, type(None), TypeVar("X")):
+                for u in (int, str, TList, Dict, bool, Base, type(None), TypeVar("X")):
                     node = CMPNode(current_node.n1, TypeNode(u), "any", "any")
                     post_order.insert(0, node)
                     current_node.children.append(node)
 
+        #print(post_order)
         for cmp_node in post_order:
             n1_type = get_origin(cmp_node.n1.type_) or cmp_node.n1.type_
             n2_type = get_origin(cmp_node.n2.type_) or cmp_node.n2.type_
             if cmp_node.children:
                 if cmp_node.ch_op == "any":
-                    #print("any", [ch.eq for ch in cmp_node.children])
                     ch_eq = any([ch.eq for ch in cmp_node.children])
                 else:
-                    #print("all", [ch.eq for ch in cmp_node.children])
                     ch_eq = all([ch.eq for ch in cmp_node.children] or [True])
             else:
                 ch_eq = True
             # check types only of both types are not union
             # otherwise equality was already decided by check above
-            #print("get origin", n1_type, n2_type)
-            print(n1_type, n2_type)
 
             if type(n1_type) == TypeVar and type(n2_type) == TypeVar:
-                #print("TV", type(n1_type))
                 ch_eq &= n1_type == n1_type
             elif type(n1_type) == TypeVar:
-                #print("TV1", type(n1_type))
                 if n2_type == Union:
                     ch_eq = True
                 else:
                     ch_eq = False
             elif type(n2_type) == TypeVar:
-                #print("TV2", type(n1_type))
                 if n1_type == Union:
                     ch_eq = True
                 else:
                     ch_eq = False
                 ch_eq = False
-
             elif n1_type != Union and n2_type != Union:
-                ch_eq &= n1_type == n2_type or issubclass(
-                    n1_type, n2_type
-                )
+                ch_eq &= issubclass(n1_type, n1_type)
+
+            elif n1_type != Union and n2_type == Union:
+                ch_eq &= any([issubclass(n1_type, t) for t in [int, str, bool, type(None), Base, TList]])
             cmp_node.eq = ch_eq
 
         return root_node.eq
@@ -294,34 +294,36 @@ class TypeNode:
 
 class BaseMeta(type):
     def __new__(cls, name, bases, attrs):
-        print("META NEW")
-
         # if config is provided, make sure it's subsclass of BaseConfig
+        new_attrs = {k: v for k, v in attrs.items()}
         if '_config' in attrs:
             assert TypeNode.from_type(type(attrs["_config"])) == TypeNode(BaseConfig)
-            config = attrs['_config']
+            config = new_attrs['_config']
         else:
             # if not, provide default config
             config = BaseConfig()
-            attrs["_config"] = config
+            new_attrs["_config"] = config
 
         if config.validate_set_attr:
             # if setter validation is on, use _validate_setattr_
             # or find it in class bases
-            if '_validate_setattr_' in attrs:
-                _setattr = attrs['_validate_setattr_']
+            if '_validate_setattr_' in new_attrs:
+                _setattr = new_attrs['_validate_setattr_']
             else:
                 _setattr = find_attr(bases, '_validate_setattr_')
-            attrs['__setattr__'] = _setattr
+            new_attrs['__setattr__'] = _setattr
         else:
-            if '_no_validate_setattr_' in attrs:
-                _setattr = attrs['_no_validate_setattr_']
+            if '_no_validate_setattr_' in new_attrs:
+                _setattr = new_attrs['_no_validate_setattr_']
             else:
                 _setattr = find_attr(bases, '_no_validate_setattr_')
-            attrs['__setattr__'] = attrs['_no_validate_setattr_']
+            attrs['__setattr__'] = new_attrs['_no_validate_setattr_']
 
-        annotations = attrs.get('__annotations__', {})
-        for attr in attrs:
+        annotations = new_attrs.get('__annotations__', {})
+        for attr, attrv in attrs.items():
+            # skip annotation check for methods and functions
+            if inspect.ismethod(attrv) or inspect.isfunction(attrv):
+                continue
             # attr starting with _ is considered to be private, there no checks
             # are applied
             if attr.startswith("_"):
@@ -340,11 +342,12 @@ class BaseMeta(type):
                 raise JSONIncompatibleError(f"Attribute {attr} is not json compatible")
 
         # record fields to private attribute
-        attrs['_fields'] = attrs.get('__annotations__', {})
-        #annotations['_fields'] = ClassVar[Dict[str, Any]]
+        new_attrs['_fields'] = {k: v for k, v in new_attrs.get('__annotations__', {}).items() if not k.startswith("_")}
 
-        ret = super().__new__(cls, name, bases, attrs)
+        #print("->>>>>>>>>> FIELDS", new_attrs['_fields'])
+        ret = super().__new__(cls, name, bases, new_attrs)
         ret = dataclasses.dataclass(ret, kw_only=True)
+        print("META RET FIELDS", ret._fields)
         return ret
 
 
@@ -356,30 +359,210 @@ class Base(metaclass=BaseMeta):
         return super().__setattr__(name, value)
 
     def _validate_setattr_(self, name: str, value: Any) -> None:
-        if name not in self._fields and not self._config.allow_extra:
-            raise AttributeError("{self.__class__} doesn't have attribute name")
+        
 
-        tt1 = TypeNode.from_type(type(value))
-        tt2 = TypeNode.from_type(self._fields[name])
-        if tt1 != tt2:
-            raise TypeError(f"Cannot set attribute {self.__class__}.{name} to type {type(value)}, expected {self._fields[name]}")
+        if not name.startswith("_"):
+            if name not in self._fields and not self._config.allow_extra:
+                raise AttributeError(f"{self.__class__} doesn't have attribute name")
+            #print("SETATTR", type(value))
+            #print("FIELD VALUE", self._fields[name])
+
+            vtype = value.__orig_class__ if hasattr(value, "__orig_class__") else value.__class__
+            #print("VTYPE", vtype)
+            tt1 = TypeNode.from_type(vtype)
+            tt2 = TypeNode.from_type(self._fields[name])
+            if tt1 != tt2:
+                raise TypeError(f"Cannot set attribute {self.__class__}.{name} to type {vtype}, expected {self._fields[name]}")
         return super().__setattr__(name, value)
 
     def __class_getitem__(cls, *params):
         ret = super().__class_getitem__(*params)
-        ret._params_map = dict(zip(cls.__parameters__, params))
 
-        print("-----", dir(ret), cls.__parameters__)
-        print("F", ret._fields)
+        print("cls getitem", ret, id(ret))
+        print("cls fields", ret._fields, id(ret._fields))
+        print("PARAMS", params, cls.__parameters__)
+        _params = params[0] if isinstance(params[0], tuple) else params
+        ret._params_map = dict(zip(cls.__parameters__, _params))
+
+        # Fields needs to be copied to specific subclass, otherwise
+        # it's stays shared with base class
+        new_fields = {}
         for attr, type_ in ret._fields.items():
-            print('getitem', attr, type_)
+            print("REPLACE old type", type_, id(type_))
             tn = TypeNode.from_type(type_)
             tn.replace_params(ret._params_map)
             new_type = tn.to_type()
+            print("REPLACE new type", new_type, id(new_type))
+
             if not tn.json_compatible():
                 raise JSONIncompatibleError(f"Attribute  {attr}: {new_type} is not json compatible")
-            ret._fields[attr] = new_type
+            new_fields[attr] = new_type
 
-        ret._params = params
+        ret._fields = new_fields
+
+        ret._params = params[0]
         return ret
+
+
+T = TypeVar("T")
+TK = TypeVar("TK")
+TV = TypeVar("TV")
+
+
+class TList(Base, Generic[T]):
+    _list: List[T]
+
+    def __init__(self, *args, **kwargs):
+        self._list = []
+        list.__init__(self._list, *args, **kwargs)
+
+    def __add__(self, value):
+        if TypeNode.from_type(value) != TypeNode.from_type(self):
+            raise TypeError(f"Cannot extend list {type(self)} with {type(value)}")
+        self.__add__(value)
+
+    def __contains__(self, value):
+        if TypeNode.from_type(value) != TypeNode.from_type(self):
+            raise TypeError(f"Cannot extend list {type(self)} with {type(value)}")
+        self.__contains__(value)
+
+    def __delitem__(self, x):
+        return self._list.__delitem__(x)
+
+    def __getitem__(self, x):
+        return self._list.__getitem__(x)
+
+    def __iter__(self):
+        return self._list.__iter__()
+
+    def __len__(self):
+        return self._list.__len__()
+
+    def __reversed__(self):
+        return self._list.__reversed__()
+
+    def __setitem__(self, key, value):
+        if TypeNode.from_type(value) != TypeNode.from_type(T):
+            raise TypeError(f"Cannot assign item {type(value)} to list of type {type(T)}")
+        self._list.__setitem__(key, value)
+
+    def append(self, obj: T) -> None:
+        if TypeNode.from_type(obj) != TypeNode.from_type(T):
+            raise TypeError(f"Cannot assign item {type(obj)} to list of type {type(T)}")
+        self._list.append(obj)
+
+    def clear(self):
+        return self._list.clear()
+
+    def count(self, value):
+        return self._list.count(value)
+
+    def extend(self, iterable):
+        if TypeNode.from_type(iterable) != TypeNode.from_type(self):
+            raise TypeError(f"Cannot extend list {type(self)} with {type(iterable)}")
+
+    def index(self, value, start, stop):
+        return self._list.index(value, start, stop)
+
+    def insert(self, index, obj):
+        if TypeNode.from_type(obj) != TypeNode.from_type(T):
+            raise TypeError(f"Cannot assign item {type(obj)} to list of type {type(T)}")
+        self._list.insert(index, obj)
+
+    def pop(self, *args, **kwargs):
+        return self._list.pop(*args, **kwargs)
+
+    def remove(self, *args, **kwargs):
+        return self._list.remove(*args, **kwargs)
+
+    def reverse(self):
+        return self._list.reverse()
+
+    def sort(self, *args, **kwargs):
+        return self._list.sort(*args, **kwargs)
+
+class TDict(Base, Generic[TK,TV]):
+    _dict: Dict[TK, TV]
+
+    def __contains__(self, key: TK) -> bool:
+        _tk = self._params[0]
+        _tv = self._params[1]
+        if TypeNode.from_type(type(key)) != TypeNode.from_type(_tk):
+            raise TypeError(f"Cannot check key {key} of type {type(key)} in dict of type {Dict[_tk, _tv]}")
+        return self._dict.__contains__(TK)
+
+    def __delitem__(self, key: TK):
+        _tk = self._params[0]
+        _tv = self._params[1]
+        if TypeNode.from_type(type(key)) != TypeNode.from_type(_tk):
+            raise TypeError(f"Cannot remove key {key} of type {type(key)} in dict of type {Dict[_tk, _tv]}")
+        self._dict.__delitem__(key)
+
+    def __getitem__(self, key: TK) -> TV:
+        _tk = self._params[0]
+        _tv = self._params[1]
+        if TypeNode.from_type(type(key)) != TypeNode.from_type(_tk):
+            raise TypeError(f"Cannot get item by key {key} of type {type(key)} in dict of type {Dict[_tk, _tv]}")
+        return self._dict.__getitem__(key)
+
+    def __init__(self, d):
+        self._dict = {}
+        for k, v in d.items():
+            self.__setitem__(k, v)
+
+    def __iter__(self):
+        return self._dict.__iter__()
+
+    def __len__(self):
+        return self._dict.__len__()
+
+    def __reversed__(self):
+        return self._dict.__reversed__()
+
+    def __setitem__(self, k: TK, v: TV):
+        _tk = self._params[0]
+        _tv = self._params[1]
+        if TypeNode.from_type(type(k)) != TypeNode.from_type(_tk):
+            raise TypeError(f"Cannot set item by key {k} of type {type(k)} in dict of type {Dict[_tk, _tv]}")
+        if TypeNode.from_type(type(v)) != TypeNode.from_type(_tv):
+            raise TypeError(f"Cannot set item {v} of type {type(v)} in dict of type {Dict[_tk, _tv]}")
+        self._dict.__setitem__(k, v)
+
+    def clear(self):
+        self._dict.clear()
+
+    def fromkeys(self, iterable, value):
+        pass
+
+    def get(self, key: TK, default=None):
+        if TypeNode.from_type(type(key)) != TypeNode.from_type(TK):
+            raise TypeError(f"Cannot get item by key {key} of type {type(key)} in dict of type {Dict[TK, TV]}")
+        return self._dict.get(key, default=default)
+
+    def items(self):
+        return self._dict.items()
+
+    def keys(self):
+        return self._dict.keys()
+
+    def pop(self, k:TK, d=None):
+        if TypeNode.from_type(type(k)) != TypeNode.from_type(TK):
+            raise TypeError(f"Cannot pop item by key {k} of type {type(k)} in dict of type {Dict[TK, TV]}")
+        return self._dict.pop(k, d)
+
+    def popitem(self) -> Tuple[TK, TV]:
+        return self._dict.popitem()
+
+    def setdefault(self, key, default):
+        if TypeNode.from_type(type(key)) != TypeNode.from_type(TK):
+            raise TypeError(f"Cannot setdefault for key {key} of type {type(key)} in dict of type {Dict[TK, TV]}")
+        return self._dict.setdefault(key, default)
+
+    def update(self, other):
+        if TypeNode.from_type(type(other)) != TypeNode.from_type(type(self)):
+            raise TypeError(f"Cannot update dict {Dict[TK, TV]} with type {type(other)}")
+        self._dict.update(other)
+
+    def values(self):
+        return self._dict.values()
 
--- tests/test_base.py
+++ tests/test_base.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Union, Optional, TypeVar, Generic
 
 import pytest
 
-from pytraction.base import Base, JSONIncompatibleError
+from pytraction.base import Base, JSONIncompatibleError, TList, TDict
 
 # Jsonable test cases
 
@@ -24,35 +24,35 @@ def test_base_jsonable_basic_optional():
 
 def test_base_jsonable_basic_structured_ok():
     class TestC(Base):
-        l: List[int]
-        d: Dict[str, int]
+        l: TList[int]
+        d: TDict[str, int]
 
 
 def test_base_jsonable_basic_structured_optional_ok():
     class TestC(Base):
-        l: Optional[List[int]]
-        d: Dict[str, Optional[int]]
+        l: Optional[TList[int]]
+        d: TDict[str, Optional[int]]
 
 
 def test_base_jsonable_basic_structured_union_ok():
     class TestC(Base):
-        l: List[int]
-        d: Dict[str, Union[int, str]]
+        l: TList[int]
+        d: TDict[str, Union[int, str]]
 
 
 def test_base_jsonable_basic_nested_structured_ok():
     class TestC(Base):
-        l: List[Dict[str, int]]
+        l: TList[TDict[str, int]]
 
 
 def test_base_jsonable_basic_nested_structured_optional_ok():
     class TestC(Base):
-        l: List[Optional[Dict[str, Optional[int]]]]
+        l: TList[Optional[TDict[str, Optional[int]]]]
 
 
 def test_base_jsonable_basic_nested_structured_union_ok():
     class TestC(Base):
-        l: List[Union[Dict[str, int], Dict[str, str]]]
+        l: TList[Union[TDict[str, int], TDict[str, str]]]
 
 
 def test_base_jsonable_basic_clasess_ok():
@@ -76,7 +76,7 @@ def test_base_jsonable_basic_clasess_union_ok():
         i: int
 
     class TestC(Base):
-        c: Dict[str, Union[TestC1, int]]
+        c: TDict[str, Union[TestC1, int]]
 
 
 def test_generic_jsonable_ok():
@@ -163,11 +163,10 @@ def test_generic_jsonable_concrete_fail():
     class TestC1(Base, Generic[T]):
         x: T
 
-    TestC1[X]
-    assert False
+    with pytest.raises(JSONIncompatibleError):
+        TestC1[X]
 
 # setattr validation test cases
-
 
 def test_base_setattr_ok():
     class TestC(Base):
@@ -194,44 +193,121 @@ def test_base_setattr_union_ok():
     t.s = 10
 
 
-def test_base_setattr_union_ok():
-    class TestC(Base):
-        s: Union[str, int]
-
-    t = TestC(s="a")
-    t.s = 10
-
-
 def test_base_setattr_complex_ok():
     class TestC(Base):
-        s: List[int]
+        s: TList[int]
 
-    t = TestC(s=["a"])
-    t.s = [10]
+    t = TestC(s=TList(["a"]))
+    t.s = TList([10])
 
 
 def test_base_setattr_complex1_ok():
     class TestC(Base):
-        s: List[int]
+        s: TList[int]
 
-    l: List[int] = [10]
-    t = TestC(s=["a"])
+    l: TList[int] = TList([10])
+    t = TestC(s=TList(["a"]))
     t.s = l
 
 
-def test_base_setattr_complex1_ok():
+def test_base_setattr_complex2_ok():
+    T = TypeVar("T")
 
+    class TestC(Base, Generic[T]):
+        td: TDict[str, T]
+
+    d: TDict[str, int] = TDict[str, int]({"a": 10})
+
+    t = TestC[int](td=TDict[str, int]({"b": 30}))
+    t.td = d
+
+# setattr validation test cases - fail
+
+
+def test_base_setattr_fail():
+    class TestC(Base):
+        i: int
+        s: str
+
+    t = TestC(i=10, s="a")
+    with pytest.raises(TypeError):
+        t.i = "a"
+    with pytest.raises(TypeError):
+        t.s = 100
+
+def test_base_setattr_optional_fail():
+    class TestC(Base):
+        s: Optional[str]
+
+    t = TestC(s="a")
+    t.s = None
+    with pytest.raises(TypeError):
+        t.s = 100
+
+
+def test_base_setattr_union_fail():
+    class TestC(Base):
+        s: Union[str, int]
+
+    t = TestC(s=10)
+    with pytest.raises(TypeError):
+        t.s = {}
+
+
+def test_base_setattr_complex_fail():
+    class TestC(Base):
+        s: TList[str]
+
+    t = TestC(s=TList(["a"]))
+    with pytest.raises(TypeError):
+        t.s = [10]
+
+
+def test_base_setattr_complex1_fail():
+    class TestC(Base):
+        s: TList[int]
+
+    l: TList[str] = TList[str](["a"])
+    t = TestC(s=TList[int]([10]))
+    print("orig", l.__orig_class__, l.__class__)
+    print(dir(l))
+    with pytest.raises(TypeError):
+        t.s = l
+
+
+def test_base_setattr_complex2_fail():
     T = TypeVar("T")
 
     class TestList(Base, Generic[T]):
-        l: List[T]
+        l: TList[T]
+
+    print("----------------------------------")
 
     class TestC(Base):
         tl: TestList[int]
-    
-    l: List[str]  = ["a"] 
 
-    tl2: TestList[int] = TestList[int](l=l)
+    print("----------------------------------")
+    print("######### params", TestList[str]._params)
+    print("######### fields", TestList[str]._fields)
 
-    t = TestC(tl=TestList[int](l=[20]))
+    tl2: TestList[str] = TestList[str](l=TList[str](["a"]))
+
+    t = TestC(tl=TestList[int](l=TList[int]([20])))
+    with pytest.raises(TypeError):
+        t.tl = tl2
+
+def test_base_setattr_complex3_ok():
+    T = TypeVar("T")
+
+    class TestDict(Base, Generic[T]):
+        l: Dict[str, T]
+
+    class TestC(Base):
+        tl: TestDict[int]
+
+    l: Dict[str, int]  = {"a": 10}
+
+    tl2: TestDict[int] = TestDict[int](l=l)
+
+    t = TestC(tl=TestDict[int](l={"b": 30}))
     t.tl = tl2
