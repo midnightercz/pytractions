3035c78b9cfabfeee578e184421e86d7bfcbdffa
midnightercz <712020:c86e8cc3-09b5-459a-9957-4db8fa992085@bots.bitbucket.org>
2023-10-01 23:00:34 +0200

    working loader/store

--- examples/tractions/relations.py
+++ examples/tractions/relations.py
@@ -1,12 +1,17 @@
-import couchdb
+from uuid import uuid4
+
 import dataclasses
 
 from typing import Any
 
 from typing import Optional, Union, Type, List, TypeVar, Generic, Dict
 
+import pytraction.base
 from pytraction.base import Base, Traction, TDict, TList, In, Out, Res, Arg, TypeNode, ANY
 from pytraction.tractor import Tractor
+
+
+import couchdb
 
 
 T = TypeVar('T')
@@ -29,10 +34,31 @@ class ModelDefinition(Base):
     fields: TDict[str, ANY]
 
 
+class Modelizer(Base):
+    _models: Dict[str, Model] = dataclasses.field(default_factory=dict)
+
+    def model_from_definition(self, definition: ModelDefinition) -> Type[Model]:
+        if definition.model_name not in self._models:
+            print("not cached", definition.model_name)
+            annot = dict(definition.fields.items())
+            annot['model_name'] = str
+            annot['uid'] = str
+            self._models[definition.model_name] = type(
+                definition.model_name, (Model,), {
+                    "__annotations__": annot,
+                    'model_name': definition.model_name,
+                    "uid": dataclasses.field(default_factory=lambda: str(uuid4().hex))}
+            )
+        mod = self._models[definition.model_name]
+        setattr(pytraction.base, definition.model_name, mod)
+        return mod
+
+
 class ModelStore(Base):
     _server: Optional[couchdb.Server] = None
     _db: Optional[couchdb.Database] = None
     uri: str
+    modelizer: Modelizer
 
     @property
     def server(self):
@@ -45,7 +71,8 @@ class ModelStore(Base):
         if not self._db:
             try:
                 self._db = self.server.create("models")
-            except Exception:
+            except Exception as e:
+                print(e)
                 pass
             self._db = self.server["models"]
         return self._db
@@ -63,10 +90,10 @@ class ModelStore(Base):
                 m_out[f] = {}
                 for k, v in getattr(model, f).items():
                     stack.append((v, k, m_out[f]))
-            elif isinstance(f, Ref):
-                if isinstance(f.model, str):
+            elif TypeNode.from_type(ftype) == TypeNode(Ref[ANY]):
+                if isinstance(getattr(model, f).model, str):
                     m_out[f] = getattr(model, f).model
-                elif isinstance(f.model, Model):
+                elif isinstance(getattr(model, f).model, Model):
                     m_out[f] = getattr(model, f).model.uid
                 else:
                     m_out[f] = None
@@ -83,7 +110,7 @@ class ModelStore(Base):
                 c_out[parent_key] = {}
                 for k, v in current.items():
                     stack.append((v, k, c_out[parent_key]))
-            elif isinstance(current, Ref):
+            elif TypeNode.from_type(ftype) == TypeNode(Ref[ANY]):
                 if isinstance(current.model, str):
                     m_out[f] = getattr(current, f).model
                 elif isinstance(current.model, Model):
@@ -94,10 +121,13 @@ class ModelStore(Base):
                 c_out[parent_key] = current.model.uid
             else:
                 c_out[parent_key] = current
-        self.db.save(out)
+        print("out", out)
+        out['_id'] = out['data']['uid']
+        _id, rev = self.db.save(out)
+        model.uid = _id
 
     def load_model_definition(self, mod_name: str):
-        model_definition_dict = self.db.find({'selector': {'type': 'ModelDefinition', 'name': mod_name}})[0]
+        model_definition_dict = next(self.db.find({'selector': {'type': 'ModelDefinition', 'name': mod_name}}))
         return ModelDefinition(
             model_name=model_definition_dict['name'],
             fields=TDict[str, ANY]({fname: TypeNode.from_json(ftype).to_type() for fname, ftype in model_definition_dict['fields'].items()})
@@ -107,12 +137,7 @@ class ModelStore(Base):
         self.db.create(
             {"type": "ModelDefinition",
              "name": model_definition.model_name,
-             "fields": {fname: TypeNode.from_type(ftype).to_json() for fname, ftype in model_definition.items()}}
-        )
-
-        return ModelDefinition(
-            model_name=model_definition_dict['name'],
-            fields=TDict[str, ANY]({fname: TypeNode.from_json(ftype).to_type() for fname, ftype in model_definition_dict['fields'].items()})
+             "fields": {fname: TypeNode.from_type(ftype).to_json() for fname, ftype in model_definition.fields.items()}}
         )
 
     def load_model(self, uid: str, max_depth: int) -> Model:
@@ -121,19 +146,21 @@ class ModelStore(Base):
         i = 0
         mfields = mdict['data']
         stack = [{"fields": mfields, "parent": None, "parent_key": None, "type": model_definition}]
-        pre_order = []
+        pre_order = [stack[0]]
         while stack:
             stack_entry = stack.pop()
-            if TypeNode.from_type(stack_entry['type']) == TypeNode.from_type(ModelDefinition):
-                for fname, ftype in model_definition._fields.items():
-                    stack.append(
-                        {"fields": efields[fname][n],
-                         "parent": stack_entry['fields'],
-                         "parent_key": fname,
-                         "type": ftype
-                         }
-                    )
-                    pre_order.insert(0, stack[-1])
+            if isinstance(stack_entry['type'], ModelDefinition):
+                #print("SE modddef", stack_entry)
+                for fname, ftype in stack_entry['type'].fields.items():
+                    if ftype not in (str, int, bool, float):
+                        stack.append(
+                            {"fields": stack_entry['fields'][fname],
+                             "parent": stack_entry['fields'],
+                             "parent_key": fname,
+                             "type": ftype
+                             }
+                        )
+                        #pre_order.insert(0, stack[-1])
             elif TypeNode.from_type(stack_entry['type']) == TypeNode.from_type(TDict[str, ANY]):
                 for k, v in stack_entry['fields'][fname].items():
                     stack.append(
@@ -155,22 +182,39 @@ class ModelStore(Base):
                     )
                     pre_order.insert(0, stack[-1])
             elif TypeNode.from_type(stack_entry['type']) == TypeNode.from_type(Ref[ANY]):
+                #print("SE ref", stack_entry)
                 rdict = self.db.get(stack_entry['fields'])
-                rmodel_definition = self.load_model_definition(mdict['model_name'])
+                rmodel_definition = self.load_model_definition(rdict['model_name'])
                 rfields = rdict['data']
                 stack.append(
                     {"fields": rfields,
-                     "parent": stack_entry['fields'],
+                     "parent": stack_entry['parent'],
                      "parent_key": stack_entry['parent_key'],
                      "type": rmodel_definition}
                 )
                 pre_order.insert(0, stack[-1])
 
+        #print(pre_order)
+        #print("--")
+        root_model = None
+        loaded_models = []
+        model_by_uid = {}
         while pre_order:
             entry = pre_order.pop(0)
-            if TypeNode.from_type(entry['type']) == TypeNode.from_type(ModelDefinition):
+            #print(entry)
+            if isinstance(entry['type'], ModelDefinition):
+                #print("mod def")
                 modelcls = self.modelizer.model_from_definition(entry['type'])
-                entry['parent'][entry['parent_key']] = modelcls(**entry['fields'])
+                if entry['parent']:
+                    if entry['fields']['uid'] in model_by_uid:
+                        model = model_by_uid[entry['fields']['uid']]
+                    else:
+                        model=modelcls(**entry['fields'])
+                        model_by_uid[entry['fields']['uid']] = model
+                    loaded_models.append(model)
+                    entry['parent'][entry['parent_key']] =  Ref[modelcls](model=model)
+                else:
+                    root_model=modelcls(**entry['fields']) 
             elif TypeNode.from_type(entry['type']) == TypeNode.from_type(TList[ANY]):
                 tlist = entry['type'](entry['fields'])
                 entry['parent'][entry['parent_key']] = tlist
@@ -178,43 +222,10 @@ class ModelStore(Base):
                 tdict = entry['type'](entry['fields'])
                 entry['parent'][entry['parent_key']] = tdict
             else:
-                entry['parent'][entry['parent_key']] = entry['fields']
+                if entry['parent']:
+                    entry['parent'][entry['parent_key']] = entry['fields']
 
-        print(entry)
-
-
-class Modelizer(Base):
-    _models: Dict[str, Model] = dataclasses.field(default_factory=dict)
-
-    def model_from_definition(self, definition: ModelDefinition) -> Type[Model]:
-        if definition.model_name not in self._models.items():
-            self._models[definition.model_name] = type(
-                definition.model_name, (Model,), {"__annotations__": definition.fields}
-                )
-        return self._models[definition.model_name]
-
-
-class ModelDefinitionToDbForm(Traction):
-    i_model: In[ModelDefinition]
-    o_out: Out[DB_COMPATIBLE] = Out[DB_COMPATIBLE](data=None)
-
-    def _run(self, on_update=None):
-        fields = {}
-        out = {"type": "ModelDefinition", "name": self.i_model.data.name, "fields": fields}
-        for fname, ftype in self.in_model.fields.items():
-            fields[fname] = TypeNode.from_type(ftype).to_json()
-        self.o_out.data = out
-
-
-class ModelDefinitionFromDbForm(Traction):
-    i_in: In[DB_COMPATIBLE]
-    o_out: Out[ModelDefinition] = ModelDefinition(model_name="", fields=TDict[str, ANY]({}))
-
-    def _run(self, on_update=None):
-        self.o_out.data = ModelDefinition(
-            name=self.i_in.data["name"],
-            fields={fname: TypeNode.from_json(ftype).to_type() for fname, ftype in self.i_in.data["fields"].items()},
-        )
+        return (root_model, loaded_models)
 
 
 class ModelToDbForm(Traction):
@@ -271,6 +282,7 @@ OrganizationDef = ModelDefinition(
     model_name="Organization",
     fields=TDict[str, ANY]({
         "name": str,
+        "created": Ref[Date]
     })
 )
 Organization = modelizer.model_from_definition(OrganizationDef)
@@ -285,8 +297,21 @@ PersonDef = ModelDefinition(
 )
 Person = modelizer.model_from_definition(PersonDef)
 
-model_store = ModelStore(uri="http://localhost:5984")
+model_store = ModelStore(uri="http://admin:password@localhost:5984", modelizer=modelizer)
 model_store.store_model_definition(DateDef)
 model_store.store_model_definition(OrganizationDef)
 model_store.store_model_definition(PersonDef)
 
+print("--1--")
+
+d1 = Date(date="2023-01-01T11:20:00")
+o1 = Organization(name='Organization 1', created=Ref[Date](model=d1))
+p1 = Person(name="John Doe", organization=Ref[Organization](model=o1), created=Ref[Date](model=d1))
+
+model_store.store_model(d1)
+model_store.store_model(o1)
+model_store.store_model(p1)
+
+
+p11 = model_store.load_model(p1.uid, 10)
+print(p11)
