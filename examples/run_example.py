d342146396540e9287445702617b67f531d62a18
Jindrich Luza <jluza@redhat.com>
2024-05-09 16:17:57 +0200

    Fixed bandit

--- examples/run_example.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import examples.tractors
-
-if __name__ == "__main__":
-    mod = __import__(f"examples.tractors", locals(), globals(), [sys.argv[1]], 0)
-    tmod = getattr(mod, sys.argv[1])
-    tmod.main()
-
