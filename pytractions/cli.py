acd2256054884b920fa857f97d3d71644b338de2
Jindrich Luza <jluza@redhat.com>
2025-07-10 13:35:18 +0200

    Removed web from cli commands

--- pytractions/cli.py
+++ pytractions/cli.py
@@ -1,7 +1,6 @@
 import argparse
 from . import container_runner
 from . import catalog
-from .web import run as web
 from . import runner
 
 if __name__ == "__main__":
@@ -9,7 +8,6 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(required=True, dest="command")
     container_runner.make_parsers(subparsers)
     catalog.make_parsers(subparsers)
-    web.make_parsers(subparsers)
     runner.make_parsers(subparsers)
 
     args = parser.parse_args()
